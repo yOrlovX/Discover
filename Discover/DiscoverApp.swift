dbc6d56c837a395c075adfba07ff487de1480d11
Yaroslav Orlov <ulverforest@gmail.com>
2023-05-08 12:28:28 +0300

    Initial Commit

---
+++ Discover/DiscoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoverApp.swift
+//  Discover
+//
+//  Created by Yaroslav Orlov on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiscoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
