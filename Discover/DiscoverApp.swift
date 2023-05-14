e2d8e2a4056c270b47e10c86911313000d321c08
Yaroslav Orlov <ulverforest@gmail.com>
2023-05-14 11:07:37 +0300

    union splash & onboarding

--- Discover/DiscoverApp.swift
+++ Discover/DiscoverApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DiscoverApp: App {
     var body: some Scene {
         WindowGroup {
-          MainView()
+          SplashView()
         }
     }
 }
--- Discover/Splash/SplashView.swift
+++ Discover/Splash/SplashView.swift
@@ -14,61 +14,73 @@ struct SplashView: View {
   @State private var animateTextOffset: CGFloat = -40
   @State private var opacity: Double = 0
   @State private var angle = Angle(degrees: 0)
+  @State private var startAnimation: Bool = false
   
-    var body: some View {
-      VStack {
-        HStack(spacing: 10) {
-          VStack(spacing: 20) {
-            Rectangle()
-              .foregroundColor(Colors.primaryBlue)
-              .frame(width: 25, height: 70)
-              .cornerRadius(20)
-              .opacity(opacity)
-              
-            Circle()
-              .foregroundColor(Colors.primaryBlue)
-              .frame(width: 25, height: 25)
-              .opacity(opacity)
-          }
-          .rotationEffect(Angle(degrees: 30))
+  var body: some View {
+    ZStack {
+      if startAnimation {
+        OnboardingView()
+      } else {
+        startingAnimationState
+      }
+    }
+    .onAppear {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 1.6) {
+        startAnimation = true
+      }
+    }
+  }
+}
+
+extension SplashView {
+  private var startingAnimationState: some View {
+    VStack {
+      HStack(spacing: 10) {
+        VStack(spacing: 20) {
           Rectangle()
             .foregroundColor(Colors.primaryBlue)
             .frame(width: 25, height: 70)
             .cornerRadius(20)
-            .rotationEffect(Angle(degrees: 30))
-            .offset(y: animateRectangleOffset)
             .opacity(opacity)
-          
           Circle()
             .foregroundColor(Colors.primaryBlue)
             .frame(width: 25, height: 25)
-            .offset(y: animateCircleOffset)
             .opacity(opacity)
-            
         }
-        .shadow(radius: 20)
-        .rotationEffect(angle)
-        
-        Text("Discover")
-          .font(.system(size: 30, weight: .semibold))
-          .opacity(0.6)
-          .offset(x: animateTextOffset)
-        
+        .rotationEffect(Angle(degrees: 30))
+        Rectangle()
+          .foregroundColor(Colors.primaryBlue)
+          .frame(width: 25, height: 70)
+          .cornerRadius(20)
+          .rotationEffect(Angle(degrees: 30))
+          .offset(y: animateRectangleOffset)
+          .opacity(opacity)
+        Circle()
+          .foregroundColor(Colors.primaryBlue)
+          .frame(width: 25, height: 25)
+          .offset(y: animateCircleOffset)
+          .opacity(opacity)
       }
-      .onAppear {
-        withAnimation(.linear(duration: 1.5)) {
-          animateCircleOffset = 40
-          animateRectangleOffset = 15
-          opacity = 1
-          animateTextOffset = 0
-//          angle = Angle(degrees: 360)
-        }
+      .shadow(radius: 20)
+      .rotationEffect(angle)
+      Text("Discover")
+        .font(.system(size: 30, weight: .semibold))
+        .opacity(0.6)
+        .offset(x: animateTextOffset)
+    }
+    .onAppear {
+      withAnimation(.linear(duration: 1.5)) {
+        animateCircleOffset = 40
+        animateRectangleOffset = 15
+        opacity = 1
+        animateTextOffset = 0
       }
     }
+  }
 }
 
 struct SplashView_Previews: PreviewProvider {
-    static var previews: some View {
-        SplashView()
-    }
+  static var previews: some View {
+    SplashView()
+  }
 }
